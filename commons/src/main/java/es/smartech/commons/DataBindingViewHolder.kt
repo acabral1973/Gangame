916d4796aad1dc5c7d6fc0729b2338e729b360b9
Alejandro Cabral Benavente <alejandrocabral@iMac-de-Alejandro.local>
2017-10-27 17:57:43 +0200

    Section 6.8 complete

---
+++ commons/src/main/java/es/smartech/commons/DataBindingViewHolder.kt
@@ -0,0 +1,11 @@
+package es.smartech.commons
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class DataBindingViewHolder<MODEL>(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bindItem(item: MODEL){
+
+        }
+}
